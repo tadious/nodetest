93d74d35d3f221ddd2280e6defa86da76dbcb45b
tadious <tadious@users.noreply.github.com>
2014-07-02 17:02:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
-var http = require("http");
-
-function OnRequest(request, response) 
+var fs = require('fs'),
+	http = require("http");
+	
+function loadPostLink(response)
 {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-  	
-  	if (request.url == '/ping')
-  		response.write("pong");
-  	else
-  		response.write("not found");
-
-  	response.end();
+	var static = fs.readFileSync('client-side-post.html');
+	response.writeHead(200, {"Content-Type": "html"});
+	response.end(static);
 }
+
+function OnRequest(request, response)
+{
+	if (request.method.toLowerCase() == 'post')
+	{
+		if (request.url == '/ping')
+		{
+			response.writeHead(200, {"Content-Type": "text/plain"});
+			response.write("pong");
+		}
+		else if(request.url == '/client-side-post')
+		{	
+			loadPostLink(response);
+		}
+		else
+		{
+			response.writeHead(403, {"Content-Type": "text/plain"});
+			response.write('not found');
+		}
+		response.end();
+	}
+	else
+	{
+		//Load the static file so we can use JQuery to post tp Node server
+		loadPostLink(response);
+	}
+}
+
+
 
 http.createServer(OnRequest).listen(8888);
