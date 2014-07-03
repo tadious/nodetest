26666f02979b056cea17e291ee751a47da1cc605
tadious <tadious@users.noreply.github.com>
2014-07-03 12:32:20 +0200

    Update server.js
    Writes to and reads from Redis

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var fs = require('fs'),
-	http = require("http");
+	https = require("https"),
+	redis = require("redis"),
+	options = {
+		key: fs.readFileSync('key.pem'),
+		cert: fs.readFileSync('cert.pem')
+	};
 	
+
 function loadPostLink(response)
 {
 	var static = fs.readFileSync('client-side-post.html');
@@ -10,31 +16,58 @@ function loadPostLink(response)
 
 function OnRequest(request, response)
 {
+	var client = redis.createClient();
+	
+	client.on('error',function(err){
+		console.log('Error in redis.... ' + err);
+	});
+	
 	if (request.method.toLowerCase() == 'post')
 	{
-		if (request.url == '/ping')
+		switch(request.url)
 		{
-			response.writeHead(200, {"Content-Type": "text/plain"});
-			response.write("pong");
+			case '/ping':
+				response.writeHead(200, {"Content-Type": "text/plain"});
+				response.write("pong");
+				response.end();
+			break;
+			
+			case '/client-side-post':
+				loadPostLink(response);
+			break;
+			
+			case '/toredis':
+				response.writeHead(200, {"Content-Type": "text/plain"});
+		
+				client.hset(["test_repo","some-key","This is REDIS!!!"], function(err, reply){
+					client.end();
+					response.end(reply);
+				});
+			break;
+			
+			case '/fromredis':
+				response.writeHead(200, {"Content-Type": "text/plain"});
+				
+				client.hget(["test_repo","some-key"], function(err, reply){
+					client.end();
+					response.end(reply);
+				});
+			break;
+			
+			default:
+				response.writeHead(403, {"Content-Type": "text/plain"});
+				response.write('not found');
+			break;
 		}
-		else if(request.url == '/client-side-post')
-		{	
-			loadPostLink(response);
-		}
-		else
-		{
-			response.writeHead(403, {"Content-Type": "text/plain"});
-			response.write('not found');
-		}
-		response.end();
 	}
 	else
 	{
 		//Load the static file so we can use JQuery to post tp Node server
 		loadPostLink(response);
+		response.end();
 	}
 }
 
 
 
-http.createServer(OnRequest).listen(8888);
+https.createServer(options, OnRequest).listen(8888);
