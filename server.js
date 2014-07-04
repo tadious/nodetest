261e0621fe32573188dac8a3eb03ad0720508ee2
tadious <tadious@users.noreply.github.com>
2014-07-04 15:47:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,73 +1,75 @@
 var fs = require('fs'),
 	https = require("https"),
-	redis = require("redis"),
 	options = {
 		key: fs.readFileSync('key.pem'),
 		cert: fs.readFileSync('cert.pem')
-	};
+	},
+	port = process.env.PORT || 8888;
 	
-
-function loadPostLink(response)
-{
-	var static = fs.readFileSync('client-side-post.html');
-	response.writeHead(200, {"Content-Type": "html"});
-	response.end(static);
-}
-
-function OnRequest(request, response)
-{
-	var client = redis.createClient();
-	
-	client.on('error',function(err){
-		console.log('Error in redis.... ' + err);
+function onRequest(request, response) {
+    var postedData = '';
+	//Get post data
+	request.on('data', function(chunk){
+		postedData += chunk;
 	});
 	
-	if (request.method.toLowerCase() == 'post')
-	{
-		switch(request.url)
-		{
-			case '/ping':
-				response.writeHead(200, {"Content-Type": "text/plain"});
-				response.write("pong");
-				response.end();
-			break;
-			
-			case '/client-side-post':
-				loadPostLink(response);
-			break;
-			
-			case '/toredis':
-				response.writeHead(200, {"Content-Type": "text/plain"});
-		
-				client.hset(["test_repo","some-key","This is REDIS!!!"], function(err, reply){
-					client.end();
-					response.end(reply);
-				});
-			break;
-			
-			case '/fromredis':
-				response.writeHead(200, {"Content-Type": "text/plain"});
-				
-				client.hget(["test_repo","some-key"], function(err, reply){
-					client.end();
-					response.end(reply);
-				});
-			break;
-			
-			default:
-				response.writeHead(403, {"Content-Type": "text/plain"});
-				response.write('not found');
-			break;
+	request.on('end', function(){
+		var urlMatch = request.url.match(/^\/map\/(.*)/);
+		if(urlMatch) {
+			var key = urlMatch[1];
+			mapRequest(key, request.method, response, postedData);
+			return;
 		}
-	}
-	else
-	{
-		//Load the static file so we can use JQuery to post tp Node server
-		loadPostLink(response);
-		response.end();
+		
+		//test for mongo db persist
+		urlMatch = request.url.match(/^\/persist\/(.*)/);
+		if(urlMatch) {
+			var key = urlMatch[1];
+			mapRequest(key, request.method, response, postedData);
+		}else {
+			response.writeHead(404, {"Content-Type": "plain/text"});
+			response.end();
+		}
+	});
+	
+	
+}
+
+function mapRequest(key, method, response, postedData) {
+	var redis = require("redis"),
+	    client = redis.createClient();
+	
+	client.on('error', function(err){
+		console.log('Error in redis: ' + err);
+	});
+	
+	if(method == 'POST') {
+		client.hset(["map",key,postedData], function(err, reply) {
+			if(err) {
+				throw err;
+			}else {
+				console.log("value=" + postedData);
+				if(reply == 1) {
+					response.writeHead(201, {"Content-Type": "plain/text"});
+					response.end();
+				}else {
+					response.writeHead(202, {"Content-Type": "plain/text"});
+					response.end();
+				}
+			}
+		});
+		
+	}else {
+		client.hget(["map",key], function(err, reply) {
+			if(err) {
+				throw err;
+			}else {
+				response.writeHead(200, {"Content-Type": "plain/text"});
+				response.end(reply);
+			}
+		});
 	}
 }
 
-
-
-https.createServer(options, OnRequest).listen(8888);
+https.createServer(options, onRequest).listen(port);
+console.log('Listening on port ' + port);
